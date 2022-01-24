9582bd736b6142dfeae6634d8662dae34a6394fd
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-01-24 22:26:00 +0000

    Create GPRMods.lua

---
+++ GPRMods.lua
@@ -0,0 +1,5 @@
+local n = require(game:GetService("Players")[game:GetService("Players").LocalPlayer.Name].Backpack.HAKR.GSSettings)
+n.Fire.FireRate = 0 
+n.Fire.Spread = 0 
+n.Fire.MaxAmmo = 9^9
+n.Fire.ReloadTime = 0 
